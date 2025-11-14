0f7b0f46f1184044e3f722c14ae71e34f7ab50fb
Maria Z <z0559882272@gmail.com>
2025-11-14 12:00:22 +0200

    added LoginSuccess

---
+++ src/test/java/tests/LoginTests.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class LoginTests extends TestBase {
+
+    @Test
+    public void loginSuccess() {
+     app.getHelperUser().openLoginRegistrationForm();
+     app.getHelperUser().fillLoginRegistrationForm("margo@gmail.com","Mmar123456$");
+     app.getHelperUser().submitLogin();
+
+     //Assert
+    }
+
+}
