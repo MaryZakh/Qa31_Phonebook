771340b9888accdf0cee051d039052b5d8fba4cd
Maria Z <z0559882272@gmail.com>
2025-11-25 19:01:59 +0200

    added Registration class

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,9 @@
 package manager;
 
+import models.User;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -40,6 +40,12 @@ public class HelperUser extends HelperBase {
 
     }
 
+    public void fillLoginRegistrationForm(User user) {
+        type(By.name("email"), user.getEmail());
+        type(By.xpath("//input[@placeholder='Password']"), user.getPassword());
+
+    }
+
     public void submitLogin() {
         click(By.xpath("//button[text()='Login']"));
     }
@@ -55,7 +61,7 @@ public class HelperUser extends HelperBase {
     public boolean isAlertPresent(String message) {
         Alert alert = new WebDriverWait(wd, Duration.ofSeconds(10))
                 .until(ExpectedConditions.alertIsPresent());
-
+        System.out.println(alert.getText());
         if (alert != null && alert.getText().contains(message)) {
             alert.accept(); //--click ok
 //            alert.dismiss(); //--click cancel
@@ -63,5 +69,19 @@ public class HelperUser extends HelperBase {
             return true;
         }
         return false;
+    }
+
+    //*********************Registration**********************************
+
+    public void submitRegistration() {
+        click(By.xpath("//button[text()='Registration']"));
+    }
+
+    public boolean isNoContactsHereDisplayed() {
+        WebDriverWait wait = new WebDriverWait(wd, Duration.ofSeconds(5));
+        boolean res = wait.until(ExpectedConditions
+                .textToBePresentInElement(wd.findElement(By.cssSelector(".contact-page_message__2qafk>h1")),
+                        "No Contacts here!"));
+        return res;
     }
 }
---
+++ src/test/java/tests/RegistrationTests.java
@@ -0,0 +1,69 @@
+package tests;
+
+import models.User;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class RegistrationTests extends TestBase {
+
+
+    @BeforeMethod
+    public void preCondition() {
+        //if SignOut button present --->logout
+        if (app.getHelperUser().isLogged()) {
+            app.getHelperUser().logout();
+        }
+    }
+
+    @Test
+    public void registrationSuccess() {
+        int i = (int) ((System.currentTimeMillis() / 1000) % 3600);
+        User user = new User().withEmail("don" + i + "@gmail.com").withPassword("Ddon14568$");
+
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitRegistration();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+        Assert.assertTrue(app.getHelperUser().isNoContactsHereDisplayed());
+    }
+
+    @Test(description = "Bug report #23456 Fixed")
+    public void registrationWrongEmail() {
+        User user = new User().withEmail("dongmail.com").withPassword("Ddon14568$");
+
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitRegistration();
+
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password format"));
+    }
+
+
+    @Test
+    public void registrationWrongPassword() {
+        User user = new User().withEmail("don@gmail.com").withPassword("Ddon145");
+
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitRegistration();
+
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("Wrong email or password format"));
+    }
+
+    @Test
+    public void registrationExistsUser() {
+        User user = new User().withEmail("margo@gmail.com").withPassword("Mmar123456$");
+
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm(user);
+        app.getHelperUser().submitRegistration();
+
+        Assert.assertTrue(app.getHelperUser().isAlertPresent("User already exist"));
+    }
+
+
+
+
+}
